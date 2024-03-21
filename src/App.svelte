1d377111938011ade14a12949d87da3f55c7ab2c
GiuDiMax <giuseppedimauro95@gmail.com>
2024-03-21 11:05:47 +0100

    edit

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,17 @@
 <script>
+  import Cookies from 'js-cookie';
+
   let data = {}
   let message = ""
-  const baseUrl = "//statsboxd-workers.giudimax.workers.dev/"
-  //const baseUrl = " http://127.0.0.1:8787/"
+  //const baseUrl = "//worker.statsboxd.eu.org/"
+  const baseUrl = " http://127.0.0.1:8787/"
 
   async function getLbdWatched(username, id="", limit=0, offset=0, session=""){
     let watched = []
     let keepGoing = true;
     let json
     let resp
+    let update
     while (keepGoing) {
       resp = await fetch(baseUrl + 'data?username='+username+"&id="+id+'&limit='+limit+'&offset='+offset +'&session='+session, {headers: {"Accept-Encoding": "br"}});
       json = await resp.json()
@@ -16,12 +19,25 @@
       offset = json['offset']
       session = json['session']
       watched = watched.concat(json['watched'])
+      update = json['update']
       id = json['id']
       if (offset === 0){
         keepGoing = false;
       }
     }
-    return {"id": id, "watched": watched, "session": session}
+    return {"id": id, "watched": watched, "session": session, "update": update}
+  }
+
+  async function getLbdUpdate(tmpdata){
+    let response = tmpdata
+    const resp = await fetch(baseUrl + 'update?id='+tmpdata['id']+'&update='+tmpdata['update']+'&session=', {headers: {"Accept-Encoding": "br"}});
+    //const resp = await fetch(baseUrl + 'update?id='+tmpdata['id']+'&update='+"2024-03-20T09:34:21.322Z"+'&session=', {headers: {"Accept-Encoding": "br"}});
+    const json = await resp.json()
+    response['watched'] = tmpdata['watched'].concat(json['watched'].filter((item) => !tmpdata['watched'].some((e) => e._id === item._id)))
+    response['update'] = json['update']
+    response['session'] = json['session']
+    //return tmpdata
+    return response
   }
 
   async function getStats(data){
@@ -37,23 +53,31 @@
 
   async function main() {
     try {
-      //const username = "dekflims"
       const username = location.pathname.split('/')[2]
-      console.log(username)
       if (username === '' || username === null){
         message = "Insert your username in the site path."
-      }else{
-        //await Promise.all([getData(), getDB()]);
-        const tmpdata = await getLbdWatched(username);
-        console.log("FINAL: " + tmpdata['watched'].length)
-        data = await getStats(tmpdata);
       }
-    } catch (error) {
+      else {
+        const localStorageData = localStorage.getItem(username)
+        let tmpdata
+        if(localStorageData !== null && localStorageData !== "undefined") {
+          tmpdata = JSON.parse(localStorageData)
+          console.log("Read from localStorage")
+          tmpdata = await getLbdUpdate(tmpdata)
+        }else{
+          tmpdata = await getLbdWatched(username)
+        }
+        localStorage.setItem(username, JSON.stringify(tmpdata))
+        data = await getStats(tmpdata)
+      }
+    }
+    catch (error) {
       console.error("Error:", error);
     }
   }
 
   main();
+
 </script>
 
 
